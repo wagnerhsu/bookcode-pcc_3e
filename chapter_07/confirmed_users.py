b3afb24eb8732a5180bdfc5844c199fb237c7e6b
Wagner Xu <wagnerhsu@hotmail.com>
2025-03-26 08:43:04 +0800

    feat: format code

--- chapter_07/confirmed_users.py
+++ chapter_07/confirmed_users.py
@@ -9,7 +9,7 @@ while unconfirmed_users:
     current_user = unconfirmed_users.pop()
     print(f"Verifying user: {current_user.title()}")
     confirmed_users.append(current_user)
-    
+
 # Display all confirmed users.
 print("\nThe following users have been confirmed:")
 for confirmed_user in confirmed_users:
